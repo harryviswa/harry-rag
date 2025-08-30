50d954f5bdd8024bb1a90ae8c21a7b0fb2b7f654
Hariharan Viswanathan <39394077+harryviswa@users.noreply.github.com>
2025-08-30 04:59:26 +0000

    added sqlite dependency changes

--- app.py
+++ app.py
@@ -1,3 +1,7 @@
+__import__('pysqlite3')
+import sys
+sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+
 import streamlit as st
 from openai import OpenAI
 from prompts import qa_testcase_prompt, qa_prompt, qa_strategy_prompt
