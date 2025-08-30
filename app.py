e6de653682e3e5a472926d783a39227c32ef71f1
Hariharan Viswanathan <39394077+harryviswa@users.noreply.github.com>
2025-08-30 05:31:12 +0000

    minor fix

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 __import__('pysqlite3')
 import sys
 sys.modules['sqlite3'] = sys.modules.pop('pysqlite3')
+import sqlite3
+print("SQLite version:", sqlite3.sqlite_version)
 
 import streamlit as st
 from openai import OpenAI
